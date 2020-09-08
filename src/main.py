1b66db2c31ba0df3b9c95986bbb79d9b0116e84d
sergioadll <sergio.adll@hotmail.com>
2020-09-08 16:24:53 +0000

    classes-defined-crud-defined-for-user-and-watchlist-stock-missing-put-modify

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, WatchList, Stock, ElementList
 #from models import Person
 
 app = Flask(__name__)
@@ -30,16 +30,157 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
+# USER CRUD
+# USER CRUD
+# USER CRUD#
 @app.route('/user', methods=['GET'])
-def handle_hello():
+def get_users():
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+    users = User.query.all()
+    all_people = list(map(lambda x: x.serialize(), users))
 
-    return jsonify(response_body), 200
+    return jsonify(all_people), 200
+@app.route('/user', methods=['POST'])
+def create_users():
+    request_user=request.get_json()
+    user1 = User(email=request_user["email"], password=request_user["password"], name=request_user["name"])
+    db.session.add(user1)
+    db.session.commit()
+
+    return jsonify("User: "+ user1.email+", created"), 200
+
+@app.route('/user/<int:user_id>', methods=['PUT'])
+def update_users(user_id):
+    request_user=request.get_json()
+    user1 = User.query.get(user_id)
+    if user1 is None:
+        raise APIException('User not found', status_code=404)
+
+    if "email" in request_user:
+        user1.email = request_user["email"]
+    if "password" in request_user:
+        user1.password = request_user["password"]
+
+    db.session.commit()
+    return jsonify("User Updated"), 200
+
+@app.route('/user/<int:user_id>', methods=['DELETE'])
+def delete_users(user_id):
+    request_user=request.get_json()
+    user1 = User.query.get(user_id)
+    if user1 is None:
+        raise APIException('User not found', status_code=404)
+    db.session.delete(user1)
+    db.session.commit()
+
+    db.session.commit()
+    return jsonify("User deleted"), 200
+
+
+# WATCHLIST CRUD
+# WATCHLIST CRUD
+# WATCHLIST CRUD
+@app.route('/watchlist', methods=['GET'])
+def get_watchlists():
+
+    watchLists = WatchList.query.all()
+    all_watchLists = list(map(lambda x: x.serialize(), watchLists))
+
+    return jsonify(all_watchLists), 200
+
+@app.route('/watchlist', methods=['POST'])
+def create_watchlist():
+    request_watchlist=request.get_json()
+    watchlist1 = WatchList(user_id=request_watchlist["user_id"],name=request_watchlist["name"])##cambiar
+    db.session.add(watchlist1)
+    db.session.commit()
+
+    return jsonify("Watchlist: "+ watchlist1.name+", created"), 200
+
+@app.route('/watchlist/<int:watchlist_id>', methods=['PUT'])
+def update_watchlist(watchlist_id):
+    request_watchlist=request.get_json()
+    watchlist1 = WatchList.query.get(watchlist_id)
+    if watchlist1 is None:
+        raise APIException('Watchlist not found', status_code=404)
+
+    if "user_id" in request_watchlist:
+        watchlist1.user_id = request_watchlist["user_id"]
+    if "name" in request_watchlist:
+        watchlist1.name = request_watchlist["name"]
+
+    db.session.commit()
+    return jsonify("Watchlist Updated"), 200
+
+@app.route('/watchlist/<int:watchlist_id>', methods=['DELETE'])
+def delete_watchlist(watchlist_id):
+    request_watchList=request.get_json()
+    watchList1 = WatchList.query.get(watchlist_id)
+    if watchList1 is None:
+        raise APIException('Watchlist not found', status_code=404)
+    db.session.delete(watchList1)
+    db.session.commit()
+
+    db.session.commit()
+    return jsonify("Watchlist deleted"), 200
+
+# STOCK CRUD
+# STOCK CRUD
+# STOCK CRUD
+
+@app.route('/stock', methods=['GET'])
+def get_stocks():
+
+    stocks = Stock.query.all()
+    all_stocks = list(map(lambda x: x.serialize(), stocks))
+
+    return jsonify(all_stocks), 200
+
+@app.route('/stock', methods=['POST'])
+def create_stock():
+    request_stock=request.get_json()
+    stock1 = Stock(name=request_stock["name"],symbol=request_stock["symbol"])##cambiar
+    db.session.add(stock1)
+    db.session.commit()
+
+    return jsonify("Stock: "+ stock1.name+", created"), 200
+
+@app.route('/stock/<int:stock_symbol>', methods=['PUT'])
+def update_stock(stock_symbol):
+    request_stock=request.get_json()
+    stock1 = Stock.query.get(stock_symbol)
+    if stock1 is None:
+        raise APIException('Stock not found', status_code=404)
+
+    if "symbol" in request_stock:
+        stock1.symbol = request_stock["symbol"]
+    if "name" in request_stock:
+        stock1.name = request_stock["name"]
+
+    db.session.commit()
+    return jsonify("Stock Updated"), 200
+
+@app.route('/stock/<int:stock_id>', methods=['DELETE'])
+def delete_stock(stock_id):
+    request_stock=request.get_json()
+    stock1 = Stock.query.get(stock_id)
+    if stock1 is None:
+        raise APIException('Stock not found', status_code=404)
+    db.session.delete(stock1)
+    db.session.commit()
+
+    db.session.commit()
+    return jsonify(stock1.name + " deleted"), 200
+
+
+
+
+
+
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
     app.run(host='0.0.0.0', port=PORT, debug=False)
+
+
--- src/models.py
+++ src/models.py
@@ -1,19 +1,78 @@
 from flask_sqlalchemy import SQLAlchemy
+import enum
 
 db = SQLAlchemy()
+#class UserType(enum.Enum):
+#    admin = 1
+#    trader = 0
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    name = db.Column(db.String(80), unique=False, nullable=False)
+    last_name = db.Column(db.String(80), unique=False, nullable=False)
+    admin = db.Column(db.Boolean, unique=False, nullable=False, default=False)
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<User %r>' % self.email
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
+            "name": self.name,
+            "last_name": self.last_name,
+            "admin":self.admin
+            # do not serialize the password, its a security breach
+        }
+class WatchList(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, nullable=False)
+    name = db.Column(db.String(120), unique=False, nullable=False)
+
+
+    def __repr__(self):
+        return '<WatchList %r>' % self.name
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_id":self.user_id,
+            "name": self.name,
+            # do not serialize the password, its a security breach
+        }    
+
+class Stock(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=False, nullable=False)
+    symbol = db.Column(db.String(12))
+
+
+    def __repr__(self):
+        return '<Stock %r>' % self.symbol
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name":self.name,
+            "symbol": self.symbol,
+            # do not serialize the password, its a security breach
+        }
+ 
+class ElementList(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    watchlist_id = db.Column(db.Integer)
+    stock_symbol = db.Column(db.String(12))
+
+
+    def __repr__(self):
+        return '<ElementList %r>' % self.stock_symbol
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "watchlist_id":self.watchlist_id,
+            "stock_symbol": self.stock_symbol,
             # do not serialize the password, its a security breach
         }
